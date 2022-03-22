b2070bb838895d12fa6cf5e3d9dc58f7ff94de69
leminhhung2002 <96687620+leminhhung2002@users.noreply.github.com>
2022-03-22 19:57:29 +0700

    hi

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Mai';
+const giftUrl = '';
+const eventName = 'Happy Birthday ';
+const titleCard = 'Tặng bạn Maiii';
+const contentCard = 'Chúc Maiii sinh nhật vui vẻ nhé,chúc Maiii luôn luôn hạnh phúc ngày nào cũng như ngày sinh nhật,luôn luôn giỏi giang ,luôn luôn xinh gái và lúc nào cũng đáng yêu như bài nhạc này nha hihi <3 ';
+
+const giftImage = '1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
